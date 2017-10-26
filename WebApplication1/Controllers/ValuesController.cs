92ff7d3d45918b8b6730b7a02e018197ca8cea79
Vijay Rakshith <vijay.rakshith@pwc.com>
2017-10-26 17:50:07 +0530

    dsfd

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Tiger", "Lion", "Elephant", "Camel", "Cow", "Sheep", "Parrot","fish","pig","swan", "horse", "dinasour" };
+            return new string[] { "Tiger", "Lion", "Elephant", "Camel", "Cow", "Sheep", "Parrot","fish","pig","swan", "horse", "dinasour", "Mango" };
         }
 
         // GET api/values/5
